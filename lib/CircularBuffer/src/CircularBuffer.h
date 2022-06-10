3f3eb011ca46a6390ff3b97b9c4a05cbb7578a66
RoguedBear <39442192+RoguedBear@users.noreply.github.com>
2022-06-10 22:21:43 +0530

    add circular buffer code, that hopefully works?
    unit tests incoming!

---
+++ lib/CircularBuffer/src/CircularBuffer.h
@@ -0,0 +1,24 @@
+#ifndef _CIRCULAR_BUFFER
+#define _CIRCULAR_BUFFER
+
+template <class T> class CircularBuffer {
+  private:
+    int __buffer_size;
+    T **__buffer;
+    int head, tail;
+
+  public:
+    /**
+     * @param buffer_size size of the buffer to allocate. reccomended to keep
+     * something in power of 2
+     */
+    CircularBuffer(int buffer_size) {}
+
+    /**
+     * @return boolean if the buffer is empty or has something
+     */
+    bool    is_empty();
+    bool    push(const T &value);
+    const T pop();
+};
+#endif
