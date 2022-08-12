8c30e3c3a2c5141ad239330b4ff31708d4e28634
RoguedBear <39442192+RoguedBear@users.noreply.github.com>
2022-08-12 20:52:35 +0530

    add config variable to specify touch threshold

--- include/CONFIG.h
+++ include/CONFIG.h
@@ -7,4 +7,5 @@
 
 #define BUFFER_SIZE          4   // buffer size to maintain
 #define KEY_PRESS_TIMEOUT_ms 400 // delay in milliseconds between key presses
+#define TOUCH_THRESHOLD      40  // touch threshold used to detec touch
 #endif
