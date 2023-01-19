4363783fd2911471b71fe25f5f90a26dab25038c
RoguedBear <39442192+RoguedBear@users.noreply.github.com>
2023-01-19 16:12:44 +0530

    dunno why this part was never committed

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@ BleKeyboard bleKeyboard(KB_BLUETOOTH_NAME, KB_BLUETOOTH_MANUFACTURER_NAME,
                         KB_BLUETOOTH_BATTERY_LEVEL);
 
 CircularBuffer<key_press_timestamp_t> buffer(BUFFER_SIZE);
-key_press_timestamp_t *               discarded_ghost_touch;
+key_press_timestamp_t                 discarded_ghost_touch;
 unsigned long                         __led_last_on = 0;
 
 void test_ISR_handler_arg(void *obj) {
@@ -71,23 +71,24 @@ void loop() {
             unsigned long         old_last_touch = key_obj->last_touch_ms;
 
             /* Ghost touch prevention workaround */
-            if (!buffer.is_empty() || discarded_ghost_touch) {
+            if (!buffer.is_empty() || discarded_ghost_touch.obj) {
                 key_press_timestamp_t next_timestamp;
                 if (!buffer.is_empty()) {
                     next_timestamp = buffer.poll();
                 }
                 // if we already discarded a ghost touch, check if the pins are
                 // different
-                if (discarded_ghost_touch &&
-                    discarded_ghost_touch->obj->pin != timestamp.obj->pin) {
-                    next_timestamp = *discarded_ghost_touch;
+                if (discarded_ghost_touch.obj &&
+                    discarded_ghost_touch.obj->pin != timestamp.obj->pin) {
+                    next_timestamp = discarded_ghost_touch;
                 }
 
                 unsigned long delta = next_timestamp.time_recorded_ms -
                                       timestamp.time_recorded_ms;
-                if (delta <= 1) { // ghost touches are recorded at the exact
-                                  // same millisecond
-                    discarded_ghost_touch = &timestamp;
+                Serial.printf("path delta: %lu\n", delta);
+                if (delta <= 100) { // ghost touches are recorded at the exact
+                                    // same millisecond
+                    discarded_ghost_touch = timestamp;
                     Serial.printf("Discarded ghost touch: %c, delta:%lu, "
                                   "current, next millis:%lu, %lu\n",
                                   timestamp.obj->letter_to_press, delta,
@@ -99,7 +100,7 @@ void loop() {
                     return;
                 }
             }
-            discarded_ghost_touch = NULL;
+            discarded_ghost_touch.obj = NULL;
 
             if (key_obj->process_touch(timestamp)) {
                 bleKeyboard.write(key_obj->letter_to_press);
